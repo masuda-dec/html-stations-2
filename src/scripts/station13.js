1458f5da1936e700fa342623ca562c64a5b7b149
masudatakeru <masuda183286@gmail.com>
2022-10-29 16:21:10 +0900

    station13

--- src/scripts/station13.js
+++ src/scripts/station13.js
@@ -1,4 +1,22 @@
-function changeBackgroundColor(e) {
-  // ここに背景色を変える処理を書く。
+let check3 = true;
+const btn = document.querySelector('#check');
 
-}
+btn.addEventListener('click', ( ) => {
+let target = document.querySelector('#text');
+  if (check3) {
+    check3 = false;
+    target.style.backgroundColor = '#ff0000';
+  } else {
+    check3 = true;
+    target.style.backgroundColor = '';
+  }
+});
+/*
+function changeBackgroundColor(text) {
+  // ここに背景色を変える処理を書く。
+  if(check) {
+    var obj = document.getElementById(text);
+    obj.style.backgroundColor = '#ff0000'
+  }
+  
+}*/
