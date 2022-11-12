85c4aec091365172a133d93b40f3cf3cbe755fc5
masudatakeru <masuda183286@gmail.com>
2022-11-12 20:14:00 +0900

    station15

--- src/scripts/station15.js
+++ src/scripts/station15.js
@@ -2,9 +2,22 @@ function displayList(){
   //alert("test");
   //test = document.getElementById("test");
   var fruits = document.getElementById("fruits");
+  var ul_element = document.createElement('ul');
+  var li_element1 = document.createElement('li');
+  var li_element2 = document.createElement('li');
+  var li_element3 = document.createElement('li');
   const p_list = document.querySelectorAll("p");
-  console.log(p_list);
-  for (i in p_list){
-    p_list[i].innerHTML = "<li>リンゴ</li>";
-  }
+  
+  li_element1.append('apple');
+  li_element2.append('strobery');
+  li_element3.append('grape');
+
+  p_list[0].replaceWith(li_element1);
+  p_list[1].replaceWith(li_element2);
+  p_list[2].replaceWith(li_element3);
+
+  fruits.appendChild(ul_element);
+  ul_element.appendChild(li_element1);
+  ul_element.appendChild(li_element2);
+  ul_element.appendChild(li_element3);
 }
